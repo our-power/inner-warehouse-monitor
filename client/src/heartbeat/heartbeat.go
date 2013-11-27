dbbae6eb1dc02f3d894adb6bbe11b7906721708f
yongfengxia <yongfengxia@tencent.com>
2013-11-27 13:44:19 +0800

    更新:新增心跳数据定时检测，更新机器状态

--- client/src/heartbeat/heartbeat.go
+++ client/src/heartbeat/heartbeat.go
@@ -2,9 +2,10 @@ package heartbeat
 
 import (
 	"database/sql"
-	//"fmt"
+	"fmt"
 	"strings"
 	"strconv"
+	"time"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/bitly/go-nsq"
 )
@@ -13,7 +14,7 @@ type HeartBeatHandler struct {
 	db *sql.DB
 }
 
-func (h *HeartBeatHandler) HandleMessage(m *nsq.Message)(err error){
+func (h *HeartBeatHandler) HandleMessage(m *nsq.Message) (err error) {
 	/*
 	实现队列消息处理功能
 	*/
@@ -29,9 +30,59 @@ func (h *HeartBeatHandler) HandleMessage(m *nsq.Message)(err error){
 	return err
 }
 
-func NewHeartBeatHandler(dbLink *sql.DB)(heartBeatHandler *HeartBeatHandler, err error){
+func NewHeartBeatHandler(dbLink *sql.DB) (heartBeatHandler *HeartBeatHandler, err error) {
 	heartBeatHandler = &HeartBeatHandler {
 		db: dbLink,
 	}
 	return heartBeatHandler, err
+}
+
+func updateMachineStatus(h *HeartBeatHandler, registerDB *sql.DB) {
+	for {
+		c := time.Tick(3*time.Minute)
+		for cTime := range c {
+			fmt.Printf("*** %d ***\n", cTime)
+			sql := "SELECT hardware_addr, status FROM register"
+			rows, _ := registerDB.Query(sql)
+			type machineStatus struct {
+				Hardware_addr string
+				Status        int
+			}
+			// 这里的参数100应根据不同仓库机器数目进行调整
+			machineListWithStatus := make([]machineStatus,0, 100)
+			var hardwareAddr string
+			var status int
+			for rows.Next() {
+				rows.Scan(&hardwareAddr, &status)
+				machineListWithStatus = append(machineListWithStatus, machineStatus{Hardware_addr: hardwareAddr, Status: status})
+			}
+			now := time.Now()
+			dateStr := now.Format("20060102")
+			nowTimeIndex := now.Hour()*60*2 + now.Minute()*2 + now.Second()/30
+			criticalTimeIndex := nowTimeIndex - 6
+			for _, item := range machineListWithStatus {
+				if item.Status == 1 || item.Status == -1 {
+					sql = "SELECT count(*) FROM heartbeat WHERE hardware_addr = ? AND date = ? AND time_index > ?"
+					rows, _ := h.db.Query(sql, item.Hardware_addr, dateStr, criticalTimeIndex)
+					var count int
+					for rows.Next() {
+						rows.Scan(&count)
+						break
+					}
+					if count == 0 && item.Status == 1 {
+						sql = "UPDATE register SET date=?, time_index=?, status=? WHERE hardware_addr=?"
+						registerDB.Exec(sql, dateStr, nowTimeIndex, -1, item.Hardware_addr)
+					}
+					if count > 0 && item.Status == -1 {
+						sql = "UPDATE register SET date=?, time_index=?, status=? WHERE hardware_addr=?"
+						registerDB.Exec(sql, dateStr, nowTimeIndex, 1, item.Hardware_addr)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (h *HeartBeatHandler) CheckPeriodically(registerDB *sql.DB) {
+	go updateMachineStatus(h, registerDB)
 }
