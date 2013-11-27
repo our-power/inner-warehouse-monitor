8848b261d6e476a92e6724fa8e8004739aa60ab6
yongfengxia <yongfengxia@tencent.com>
2013-11-27 16:11:43 +0800

    更新：删除调试打印语句

--- client/src/cpu_usage/cpu_usage.go
+++ client/src/cpu_usage/cpu_usage.go
@@ -2,7 +2,6 @@ package cpu_usage
 
 import (
 	"database/sql"
-	"fmt"
 	"strings"
 	"strconv"
 	_ "github.com/mattn/go-sqlite3"
@@ -17,7 +16,6 @@ func (h *CPUUsageHandler) HandleMessage(m *nsq.Message)(err error){
 	/*
 	实现队列消息处理功能
 	*/
-	fmt.Printf("%s\n",m.Body)
 	bodyParts := strings.Split(string(m.Body), "\r\n")
 	time_index, err := strconv.Atoi(bodyParts[1])
 	sql := `
