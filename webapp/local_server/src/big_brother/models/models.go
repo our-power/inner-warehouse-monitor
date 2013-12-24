61d9e222d59f436ac75f1617804c7413393459ab
yongfengxia <yongfengxia@tencent.com>
2013-12-24 17:36:53 +0800

    update

--- webapp/local_server/src/big_brother/models/models.go
+++ webapp/local_server/src/big_brother/models/models.go
@@ -15,6 +15,7 @@ type Common_fields struct {
 }
 */
 
+/*
 type Cpu_usage struct {
 	Id int
 	Date string
@@ -57,42 +58,43 @@ type Heart_beat struct {
 	Hardware_addr string
 	Alive int
 }
-
+*/
 type Register struct {
-	Id int
-	Date string
-	Time_index int
-	Ip string
-	Host_name string
+	Id            int
+	Date          string
+	Time_index    int
+	Ip            string
+	Host_name     string
 	Hardware_addr string
 	Agent_version string
-	Machine_role string
-	Status int
+	Machine_role  string
+	Status        int
 }
 
+/*
 type Ping_accessibility struct {
-	Id int
-	Date string
-	Time_index int
-	Ip string
-	Host_name string
+	Id            int
+	Date          string
+	Time_index    int
+	Ip            string
+	Host_name     string
 	Hardware_addr string
-	Target_ip string
+	Target_ip     string
 	Response_time int
 }
 
 type Telnet_accessibility struct {
-	Id int
-	Date string
-	Time_index int
-	Ip string
-	Host_name string
+	Id            int
+	Date          string
+	Time_index    int
+	Ip            string
+	Host_name     string
 	Hardware_addr string
-	Target_url string
-	Status string
+	Target_url    string
+	Status        string
 }
-
-
+*/
 func InitModels() {
-	orm.RegisterModel(new(Cpu_usage), new(Mem_usage), new(Net_flow), new(Heart_beat), new(Register), new(Ping_accessibility), new(Telnet_accessibility))
+	//orm.RegisterModel(new(Cpu_usage), new(Mem_usage), new(Net_flow), new(Heart_beat), new(Register), new(Ping_accessibility), new(Telnet_accessibility))
+	orm.RegisterModel(new(Register))
 }
