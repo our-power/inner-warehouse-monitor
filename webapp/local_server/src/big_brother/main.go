a8a0b19f61d1da334f32ce45b520c2c11e7b76be
yongfengxia <yongfengxia@tencent.com>
2014-01-09 14:56:34 +0800

    updare

--- webapp/local_server/src/big_brother/main.go
+++ webapp/local_server/src/big_brother/main.go
@@ -7,19 +7,17 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/mattn/go-sqlite3"
-	"os"
-	"path"
-	"path/filepath"
 )
 
 func init() {
-	appPath, _ := filepath.Abs(os.Args[0])
-	beego.AppConfigPath = path.Join(path.Dir(appPath), "conf/app.conf")
-
 	registerDB := beego.AppConfig.String("multidb") + "register.db"
 	orm.RegisterDataBase("default", beego.AppConfig.String("dbdriver"), registerDB)
 	models.InitModels()
 	controllers.InitControllers()
+}
+
+func addInTpl(x, y int) int {
+	return x + y
 }
 
 func main() {
@@ -41,5 +39,6 @@ func main() {
 	beego.Router("/api/get_machine_accessibility_data", &controllers.ApiController{}, "GET:GetMachineAccessibilityData")
 	beego.Router("/api/status_overview", &controllers.ApiController{}, "GET:GetStatusOverview")
 
+	beego.AddFuncMap("add", addInTpl)
 	beego.Run()
 }
