4b6c08cccddb43d4d29d8d241e2e78ae309eecec
yongfengxia <yongfengxia@tencent.com>
2014-01-20 11:39:47 +0800

    更新

--- webapp/local_server/src/big_brother/static/js/admin/main.js
+++ webapp/local_server/src/big_brother/static/js/admin/main.js
@@ -2,5 +2,43 @@
  * Created by yongfengxia on 14-1-17.
  */
 $(function(){
+    $("button#add-new-user").on("click", function(e){
+        e.preventDefault();
 
+        $("#modal-for-role").modal("show");
+    });
+
+    $("button#add-new-role").on("click", function(e){
+        e.preventDefault();
+
+        $("#modal-for-role").modal("show");
+    });
+
+    $("button#modify-this-user").on("click", function(e){
+        e.preventDefault();
+
+    });
+
+    $("button#del-this-user").on("click", function(e){
+        e.preventDefault();
+
+    });
+
+    $("button#modify-this-role").on("click", function(e){
+        e.preventDefault();
+
+    });
+
+    $("button#del-this-role").on("click", function(e){
+        e.preventDefault();
+
+    });
+
+    $("button#save-user").on("click", function(e){
+        e.preventDefault();
+    });
+
+    $("button#save-role").on("click", function(e){
+        e.preventDefault();
+    });
 });
