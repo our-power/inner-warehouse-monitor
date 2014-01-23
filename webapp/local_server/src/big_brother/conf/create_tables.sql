25812089b2c17aecce3584579a8a07eb77ba26d7
yongfengxia <yongfengxia@tencent.com>
2014-01-23 17:38:35 +0800

    update

--- webapp/local_server/src/big_brother/conf/create_tables.sql
+++ webapp/local_server/src/big_brother/conf/create_tables.sql
@@ -2,4 +2,4 @@ CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, passw
 CREATE TABLE role (id INTEGER PRIMARY KEY AUTOINCREMENT, role_type TEXT, permission TEXT);
 CREATE TABLE trace (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, do_what TEXT, that_time TEXT);
 INSERT INTO role(role_type, permission) VALUES ("user_admin", "view|add|modify|del|admin");
-INSERT INTO user(name, passwd, role_id) VALUES ("root", "03941b924d12454219648d61a7b025e1", 0);
+INSERT INTO user(name, passwd, role_id) VALUES ("root", "03941b924d12454219648d61a7b025e1", 1);
